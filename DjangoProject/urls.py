a0632df958ad61a389ef8de9328180f7f0ca0700
Ranajit Nath <ranajitnath7@gmail.com>
2022-03-09 18:18:39 +0530

    V2 Core files added in Master branch
    Signed-off-by: Ranajit Nath <ranajitnath7@gmail.com>

--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -1,4 +1,4 @@
-"""CoreProject URL Configuration
+"""DjangoProject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django import http
 from django.contrib import admin
 from django.urls import path
+from django import http
 
 urlpatterns = [
-    path('health', lambda req: http.HttpResponse(200)),
     path('admin/', admin.site.urls),
+    path('health', lambda req: http.HttpResponse(200)),
+    path('', lambda req: http.HttpResponse(200)),
 ]
