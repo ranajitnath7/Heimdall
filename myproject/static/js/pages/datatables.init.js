44154ce83433b6b57dcc96d46bb5e6fc25062ada
Ranajit Nath <ranajitnath7@gmail.com>
2022-03-09 17:31:39 +0530

    V2 Core files added in Master branch
    Signed-off-by: Ranajit Nath <ranajitnath7@gmail.com>

--- myproject/static/js/pages/datatables.init.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){$("#datatable").DataTable({
-        "paging":   false,
-        "ordering": false,
-        "info":     false
-    });var a=$("#datatable-buttons").DataTable({lengthChange:!1,buttons:["copy","excel","pdf"]});$("#key-datatable").DataTable({keys:!0}),$("#selection-datatable").DataTable({select:{style:"multi"}}),a.buttons().container().appendTo("#datatable-buttons_wrapper .col-md-6:eq(0)")});
--- settings/context_processors.py
+++
@@ -1,6 +0,0 @@
-from settings.models import SiteInfo
-
-
-def site_info(request):
-    site_info = SiteInfo.objects.all().first()
-    return {'site_info': site_info}
